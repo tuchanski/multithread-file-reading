59c806389623a98b4c4decc850de4250573e88e7
Tuchanski <gui.rocha2004@gmail.com>
2024-10-24 10:34:27 -0300

    Apply Thread Pool on Main

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -17,24 +18,18 @@ public class Main {
         File dir = new File("./todosArquivos");
         files = dir.listFiles();
 
-        List<Thread> threads = new ArrayList<>();
-
         Long start = System.currentTimeMillis();
 
-        for (int i = 0; i < files.length; i++) {
-            Thread thread = countChar(files[i]);
-            if (thread != null) {
-                thread.start();
-                threads.add(thread);
-            }
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        for (File file : files){
+            executorService.submit(() -> countChar(file));
         }
 
-        for (Thread t : threads) {
-            t.join(); // Aguarda a finalização de todas as threads
-        }
-
+        executorService.shutdown();
+        executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+        
         Long end = System.currentTimeMillis();
-
         Long totalTime = end - start;
 
         getElements();
@@ -58,48 +53,40 @@ public class Main {
     }
 
     // Contagem de caracteres com threads
-    public static Thread countChar(File file) {
+    public static void countChar(File file) {
 
         try {
 
             FileReader fileReader = new FileReader(file.getAbsolutePath());
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            Thread t = new Thread() {
+            try {
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    char lineArr[] = line.toCharArray();
 
-                @Override
-                public void run() {
-                    try {
-                        String line;
-                        while ((line = bufferedReader.readLine()) != null) {
-                            char lineArr[] = line.toCharArray();
-
-                            // Sincroniza o acesso ao dicionário (pois muitas threads vão tentar alterar a chave ao mesmo tempo)
-                            synchronized (alphabet) {
-                                for (char letter : lineArr) {
-                                    char lowerCaseLetter = Character.toLowerCase(letter);
-                                    if (alphabet.containsKey(lowerCaseLetter)) {
-                                        alphabet.put(lowerCaseLetter, alphabet.get(lowerCaseLetter) + 1);
-                                    }
-                                }
+                    // Sincroniza o acesso ao dicionário (pois muitas threads vão tentar alterar a chave ao mesmo tempo)
+                    synchronized (alphabet) {
+                        for (char letter : lineArr) {
+                            char lowerCaseLetter = Character.toLowerCase(letter);
+                            if (alphabet.containsKey(lowerCaseLetter)) {
+                                alphabet.put(lowerCaseLetter, alphabet.get(lowerCaseLetter) + 1);
                             }
                         }
-
-                        bufferedReader.close();
-                        fileReader.close();
-
-                    } catch (Exception e) {
-                        System.out.println("Error: " + e.getMessage());
                     }
                 }
-            };
 
-            return t;
+                bufferedReader.close();
+                fileReader.close();
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
 
-        return null;
     }
 
 }
