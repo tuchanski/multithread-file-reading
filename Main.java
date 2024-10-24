e70ec4fbb6f53b48ac17f36a1388b75d9c3e0d3f
Tuchanski <gui.rocha2004@gmail.com>
2024-10-23 21:03:40 -0300

    Update Main structure

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
